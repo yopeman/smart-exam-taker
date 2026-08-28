c6344d0b1047162d3438ca668e8f5128c1412505
Yohanes Debebe <yopeman318@gmail.com>
2026-08-28 20:39:31 +0300

    move the routing from main.jsx to App.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,29 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import { AuthProvider } from './contexts/AuthContext'
 import App from './App.jsx'
-import Login from './components/Login'
-import SchoolsList from './components/SchoolsList'
-import ProtectedRoute from './components/ProtectedRoute'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/schools"
-            element={
-              <ProtectedRoute>
-                <SchoolsList />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
+        <App />
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
