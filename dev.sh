5b632fa132c101e84d77726d01c2b6b851ec5933
Yohanes Debebe <yopeman318@gmail.com>
2026-09-06 17:57:25 +0300

    fix pkg issues

--- dev.sh
+++ dev.sh
@@ -1,4 +1,5 @@
 (cd dependency && docker compose up) &
 (cd backend && uv run run.py) &
+(ngrok http 8000) &
 (cd frontend && npm run dev) &
-(cd mobile-app && npx expo start)
+(cd mobile-app && npx expo start) &
