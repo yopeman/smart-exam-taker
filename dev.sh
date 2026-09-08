139ddc2a17832b57074a52a9b9b0285157c8f2d7
Yohanes Debebe <yopeman318@gmail.com>
2026-09-08 11:12:38 +0300

    add local tunnel command in dev.sh

--- dev.sh
+++ dev.sh
@@ -1,5 +1,6 @@
 (cd dependency && docker compose up) &
 (cd backend && uv run run.py) &
-(ngrok http 8000) &
+# (ngrok http 8000) &
+# (npx localtunnel --port 8000 --subdomain smart-exam-taker-backend-by-yopeman-318) &
 (cd frontend && npm run dev) &
 (cd mobile-app && npx expo start) &
