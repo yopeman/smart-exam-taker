7fc26f6d44ca8343ae2fbe7e4a5b5de6a5660a63
Yohanes Debebe <yopeman318@gmail.com>
2026-09-10 13:42:16 +0300

    comment mobile running from the dev.sh

--- dev.sh
+++ dev.sh
@@ -3,4 +3,4 @@
 # (ngrok http 8000) &
 (npx localtunnel --port 8000 --subdomain smart-exam-taker-backend-by-yopeman-318) &
 (cd frontend && npm run dev) &
-(cd mobile-app && npx expo start) &
+# (cd mobile-app && npx expo start) &
